d183ce590d0443b527e7eb22f81302cfc3f55285
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2025-06-18 11:15:45 +0530

    a

---
+++ 38.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void swap(int a,int b);
+void _swap(int *a,int *b);
+
+int main() {
+    int x=2;
+    int y=5;
+    swap(x,y);
+    printf("x= %d & y=%d\n",x,y);
+    return 0;
+}    
+    //call by value
+    void swap(int a ,int b) {
+        int t=a;
+        a=b;
+        b=t;
+        printf("a=%d & b=%d\n", a,b);
+        
+    }
+    //call by reference
+    void _swap(int *a,int *b) {
+        int t=*a;
+        *a=*b;
+        *b=t;
+        printf("*a =%d & *b=%d\n",*a,*b);
+       
+    }
