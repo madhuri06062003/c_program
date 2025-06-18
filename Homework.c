ba02205c1b9685b6d5158027547f361e2ea3afe7
karampudi madhuri <madhuri06062003.kvks@gmail.com>
2025-06-18 11:58:19 +0530

    a

--- Homework.c
+++ Homework.c
@@ -19,7 +19,7 @@ int main() {
 }*/
 
 //c program to print all letters in english using pointers
-#include<stdio.h>
+/*#include<stdio.h>
 int main() {
     char ch;
     char *ptr;
@@ -35,6 +35,30 @@ int main() {
     for(ch='a';ch<='z';ch++) {
         ptr=&ch;
         printf("%c",*ptr);
+    }
+    printf("\n");
+    return 0;
+}*/
+
+//c program to print elements of array in reverse order
+#include<stdio.h>
+int main() {
+    int arr[100],n,i;
+    int *ptr;
+
+    printf("Enter number of elements : ");
+    scanf("%d\n",&n);
+
+    printf("Elements %d:\n",n);
+    for(i=0;i<n;i++) {
+        scanf("%d",&arr[i]);
+    }
+    ptr=&arr[n-1];
+
+    printf("Reverse order :\n");
+    for(i=n-1;i>=0;i--) {
+        printf("%d",*ptr);
+        ptr--;
     }
     printf("\n");
     return 0;
